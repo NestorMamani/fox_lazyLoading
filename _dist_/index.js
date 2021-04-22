1eb5c529ca6b102040247771814d84682d3d1a1a
NestorMamani <NestorMamani@users.noreply.github.com>
2021-04-22 13:34:46 +0000

    deploy: 758cd9ad227cb7800060fa113c63c81190cb93fd

---
+++ _dist_/index.js
@@ -0,0 +1,28 @@
+import { registerImage } from './lazy.js';
+
+const images = document.getElementById('images');
+const add_button = document.getElementById('add');
+const remove_button = document.getElementById('remove');
+
+let addImages = 0;
+
+add_button.addEventListener('click', () => {
+  const container = document.createElement('div');
+  container.classList.add('p-4');
+
+  const image = document.createElement('img');
+  image.classList.add('mx-auto');
+  image.width = 320;
+
+  container.append(image);
+
+  images.append(container);
+  registerImage(container);
+
+  addImages++;
+  console.log('Imágenes agregadas: ' + addImages);
+});
+
+remove_button.addEventListener('click', () => {
+  images.innerHTML = '';
+});
